7614c77ea47331674605c774bdcb6a743d3e7c2e
Орлова Анастасия <74093413+Kordova17@users.noreply.github.com>
2020-11-20 22:17:17 +0300

    Add files via upload

---
+++ Домашечка.py
@@ -0,0 +1,26 @@
+myGreeting = "Здравствуйте, я хочу рассказать немного о себе"
+print(myGreeting)
+
+myAge = "18"
+myName = "Анастасия"
+myLocation = "Великий Новгород"
+
+print('Мне',myAge,'лет,','мое имя', myName,',' 'Проживаю в городе', myLocation)
+
+myPosition = "Студент"
+myEducationalInstitution = "НСК"
+myStudyingTime = "2"
+
+print('Я',myPosition, myEducationalInstitution,',', myStudyingTime, 'курс',)
+
+myFutureProfession = "Моя будущая проффесия программист"
+myChoiceProfession = "Меня привлекает Машинное Обучение в сфере IT"
+print(myFutureProfession, myChoiceProfession)
+
+myEnding = "Спасибо за внимание!"
+
+
+
+
+
+
